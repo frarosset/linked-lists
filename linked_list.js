35bbc92dd2880a2166db1178c3362ef0def2bfe3
frarosset <37275581+frarosset@users.noreply.github.com>
2024-04-23 02:29:58 +0200

    Add toString method to LinkedList
    This will allow to better test the other methods that will be
    added next.

--- linked_list.js
+++ linked_list.js
@@ -46,20 +46,38 @@ class LinkedList{
         // the list is not empty (note: no tail = no head)
         if (this.#tail){
             this.#tail.next = newNode;
-            console.log('Old tail', this.#tail.value,' - head', this.#head.value);
+            //console.log('Old tail', this.#tail.value,' - head', this.#head.value);
             this.#tail = newNode;
-            console.log('New tail', this.#tail.value,' - head', this.#head.value);
+            //console.log('New tail', this.#tail.value,' - head', this.#head.value);
         } else {
             this.#head = newNode;
             this.#tail = newNode;
-            console.log('New list with tail', this.#tail.value,' - head', this.#head.value);
+            //console.log('New list with tail', this.#tail.value,' - head', this.#head.value);
         }
-    }    
+    }
+
+    // this method represents the LinkedList objects as strings, 
+    // allowing to print them out and preview them in the console.
+    // The format should be: ( value ) -> ( value ) -> ( value ) -> null
+    toString(){
+        let thisNode = this.#head;
+        let str = '';
+        
+        while (thisNode != null){
+            str += `( ${thisNode.value} )  -> `;
+            thisNode = thisNode.next;
+        }
+        str += 'null';
+
+        return str;
+    }
 }
 
 
 let testLinkedList = new LinkedList();
+console.log(testLinkedList.toString());
 testLinkedList.append(1);
 testLinkedList.append(2);
 testLinkedList.append(3);
 testLinkedList.append(4);
+console.log(testLinkedList.toString());
