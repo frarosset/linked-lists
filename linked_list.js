651e1d399b2ae9c9a933a53bad157ee435df617e
frarosset <37275581+frarosset@users.noreply.github.com>
2024-04-23 02:04:13 +0200

    Add setter/getter methods to Node class

--- linked_list.js
+++ linked_list.js
@@ -1,12 +1,42 @@
 class Node{
+    #value;
+    #next;
+
     constructor(value=null,next=null){
         this.#value = value;
         this.#next = next;
     }
-}
 
-class LinkedList{
-    constructor(){
-        this.#head = null;
+    get value(){
+        return this.#value;
+    }
+
+    set value(updatedValue){
+        this.#value = updatedValue;
+    }
+
+    get next(){
+        return this.#next;
+    }
+
+    set next(updatedNext){
+        this.#next = updatedNext;
     }
 }
+
+// let testNode = new Node(4);
+// console.log(testNode.next);
+// console.log(testNode.value);
+// testNode.value = 10;
+// console.log(testNode.value);
+
+class LinkedList{
+    #head;
+    #tail;
+
+    constructor(){
+        this.#head = null;
+        this.#tail = null;
+    }
+}
+
