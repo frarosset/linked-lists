aef8d9e889ca5ef0653ca76e4ca557df22879c0d
frarosset <37275581+frarosset@users.noreply.github.com>
2024-04-23 02:17:43 +0200

    Add append method to LinkedList class

--- linked_list.js
+++ linked_list.js
@@ -38,5 +38,28 @@ class LinkedList{
         this.#head = null;
         this.#tail = null;
     }
+
+    // this method adds a new node containing value to the end of the list
+    append(value){
+        let newNode = new Node(value);
+
+        // the list is not empty (note: no tail = no head)
+        if (this.#tail){
+            this.#tail.next = newNode;
+            console.log('Old tail', this.#tail.value,' - head', this.#head.value);
+            this.#tail = newNode;
+            console.log('New tail', this.#tail.value,' - head', this.#head.value);
+        } else {
+            this.#head = newNode;
+            this.#tail = newNode;
+            console.log('New list with tail', this.#tail.value,' - head', this.#head.value);
+        }
+    }    
 }
 
+
+let testLinkedList = new LinkedList();
+testLinkedList.append(1);
+testLinkedList.append(2);
+testLinkedList.append(3);
+testLinkedList.append(4);
